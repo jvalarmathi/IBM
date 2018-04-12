a0fef88b4a8613ea09c2904307ef02ad02a3fccc
Valarmathi <valar123@in.ibm.com>
2018-04-12 12:29:18 +0530

    Test3

---
+++ src/main/java/Test/Jmathi/App.java
@@ -0,0 +1,17 @@
+package Test.Jmathi;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+	@Test
+    public  void test()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
